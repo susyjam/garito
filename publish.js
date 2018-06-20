13104a570d879c384d59856e076bd8ce3a75a5d6
susyjam <susyjame@gmail.com>
2018-06-20 00:19:19 -0400

    mi primer commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../migarito.zip');
+var kuduApi = 'https://migarito.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$migarito';
+var password = 'WCow7XL5PXRwpHhwq45Gq766Rvzcj3oEitkvegShYxAx165Wbmt1HuSjamjN';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('migarito publish');
+  } else {
+    console.error('failed to publish migarito', err);
+  }
+});
